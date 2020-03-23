6928ebe19bf1afede472504b07569a70bbfec6ee
Alfredo Prince <aprince@toweredtech.com>
2020-03-23 17:24:49 -0600

    HTTPS added in controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpClient\HttpClient;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user", name="user", schemes={"https"})
      */
     public function index()
     {
